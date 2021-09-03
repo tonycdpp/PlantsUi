055e7ae049fafc6b92be323a917f61d2b9728eb7
Tony Kajtazi <tony.kajtazi@nhs.net>
2021-09-03 16:18:55 +0100

    Revert "changes on wireframe"
    This reverts commit 3e1b06334e9b1da07c80a1fa16989711cf6146db.

--- src/index.js
+++ src/index.js
@@ -4,21 +4,13 @@ import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const PlantsList = (props) => (
-  <div>
-    <Plants />
-  </div>
-
-);
 
 class Plants extends React.Component{
   render() {
     return(
-    <div className ="plants-profile" style={{ margin: '1rem'}}>
-     <img src= "https://placehold.it/75"/>
-     <div className= "info">
-      <div className="name">  Plant Name here...</div>
-      <div className= "plantInfo"> Plant information here...</div>
+    <div className ="plants-profile">
+      <div className= "info">
+      <div className ="name"> Essi Name here...</div>
      </div> 
     </div>
     );
@@ -30,7 +22,7 @@ class App extends React.Component{
     return (
       <div>
         <div className= "header">(this.props.title)</div>
-        <PlantsList/>
+        <Plants/>
         </div>
     );
   }
