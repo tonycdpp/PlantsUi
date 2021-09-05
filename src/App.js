327fa09ad3e527c185c3a6b5bc19e1ad546744af
Tony Kajtazi <tony.kajtazi@nhs.net>
2021-09-05 13:26:05 +0100

    fixed navigation routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import Users from "./components/users-login/Users";
 import UserPlants from "./components/user-plants/UserPlants";
+import UserPlantsFetcher from "./components/user-plants/UserPlantsFetcher";
 import './App.css';
 import axios from 'axios';
 import { BrowserRouter as Router, Route, useParams } from "react-router-dom";
 
 
 export default function App() {
-
-    //TODO: present a list of users to log on as
-    // separate the USer plants into a separate component
-    //once the user is clicked, load user plants component
     const [hasError, setErrors] = useState(false);
     const [userPlants, setUserPlants] = useState([]);
     const [users, setUsers] = useState([]);
-    const { userrowkey } = useParams();
-
 
     async function fetchUsers() {
 
@@ -29,27 +24,9 @@ export default function App() {
             });
     }
 
-    async function fetchUserPlants(userId) {
-
-        console.log(userrowkey);
-
-
-        const res = axios.get(`https://plants-api.azurewebsites.net/users/${userId}/plants`)
-            .then(response => {
-                setUserPlants(response.data);
-                // console.log(response.data)
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }
-
     useEffect(() => {
         fetchUsers();
-        fetchUserPlants('039c9b79-8dfc-4e9a-85ec-33f350f3bd2e');
     }, []);
-
-
 
     return (
         <React.Fragment>
@@ -60,7 +37,7 @@ export default function App() {
                             <Users data={users} />
                         </Route>
                         <Route exact path="/users/:userrowkey/plants" >
-                            <UserPlants userPlants={userPlants} />
+                            <UserPlantsFetcher />
                         </Route>
                     </div>
                 </Router>
---
+++ src/components/user-plants/UserPlantsFetcher.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import UserPlants from "./UserPlants";
+import axios from 'axios';
+import { useParams } from "react-router-dom";
+
+export default function UserPlantsFetcher() {
+    const [userPlants, setUserPlants] = useState([]);
+    const { userrowkey } = useParams();
+    console.log(userrowkey);
+
+    async function fetchUserPlants(userId) {
+        const res = axios.get(`https://plants-api.azurewebsites.net/users/${userId}/plants`)
+            .then(response => {
+                setUserPlants(response.data);
+                // console.log(response.data)
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    useEffect(() => {
+        fetchUserPlants(userrowkey);
+    }, []);
+
+    return (
+        <UserPlants userPlants={userPlants} />
+    );
+}
