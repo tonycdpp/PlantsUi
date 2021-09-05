06a5428e38c1099a2f83ea3cc4923cce08b74b77
Tony Kajtazi <tony.kajtazi@nhs.net>
2021-09-05 13:33:14 +0100

    finished refactoring

--- src/App.js
+++ src/App.js
@@ -1,47 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import Users from "./components/users-login/Users";
-import UserPlants from "./components/user-plants/UserPlants";
 import UserPlantsFetcher from "./components/user-plants/UserPlantsFetcher";
+import UsersFetcher from "./components/users-login/UsersFetcher";
 import './App.css';
-import axios from 'axios';
 import { BrowserRouter as Router, Route, useParams } from "react-router-dom";
 
-
 export default function App() {
-    const [hasError, setErrors] = useState(false);
-    const [userPlants, setUserPlants] = useState([]);
-    const [users, setUsers] = useState([]);
-
-    async function fetchUsers() {
-
-        const res = axios.get(`https://plants-api.azurewebsites.net/users`)
-            .then(response => {
-                setUsers(response.data);
-                // console.log(response.data)
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }
-
-    useEffect(() => {
-        fetchUsers();
-    }, []);
-
     return (
         <React.Fragment>
-            <main>
-                <Router>
-                    <div>
-                        <Route exact path="/" >
-                            <Users data={users} />
-                        </Route>
-                        <Route exact path="/users/:userrowkey/plants" >
-                            <UserPlantsFetcher />
-                        </Route>
-                    </div>
-                </Router>
-            </main>
+            <Router>
+                <Route exact path="/" >
+                    <UsersFetcher />
+                </Route>
+                <Route exact path="/users/:userrowkey/plants" >
+                    <UserPlantsFetcher />
+                </Route>
+            </Router>
         </React.Fragment >
     );
 }
---
+++ src/components/users-login/UsersFetcher.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import Users from "./Users";
+import axios from 'axios';
+
+export default function UsersFetcher() {
+    const [users, setUsers] = useState([]);
+    async function fetchUsers() {
+
+        const res = axios.get(`https://plants-api.azurewebsites.net/users`)
+            .then(response => {
+                setUsers(response.data);
+                // console.log(response.data)
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    return (
+        <Users data={users} />
+    );
+}
