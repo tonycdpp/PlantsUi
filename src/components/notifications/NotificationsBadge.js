9a0db3eea8a1de0da99880d91da7c3b421ac11f9
Tony Kajtazi <tony.kajtazi@nhs.net>
2021-09-15 18:56:15 +0100

    bug fix

--- src/components/notifications/NotificationsBadge.js
+++ src/components/notifications/NotificationsBadge.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import css from './NotificationsBadge.module.css'
 import bell from "./../notifications/bell.png"
 import bellhover from "./../notifications/bell_full.png"
